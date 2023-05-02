42940b49d012444855f4707fe1743485270a63ad
jiyo01 <jyyy0219@gmail.com>
2023-05-02 12:38:01 +0900

    jsp & servlet 예제

---
+++ Practice_Chap04_Servlet3/src/sec01_exam/LoginServlet.java
@@ -0,0 +1,56 @@
+package sec01_exam;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		String id = request.getParameter("user_id");
+		String pw = request.getParameter("user_pw");
+		String address = request.getParameter("user_address");
+		
+		PrintWriter out = response.getWriter();
+		out.println("<html><head></head><body>");
+		out.println("아이디 : " + id + "<br>");
+		out.println("비밀번호 : " + pw + "<br>");
+		out.println("주소 : " + address + "<br>");
+		out.println("</body></html>");
+		out.close();
+	}
+}
+
+
+
